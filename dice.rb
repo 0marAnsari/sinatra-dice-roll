f5bfb7632f63da35bc0de93f75eecb6924892390
Omar Ansari <112027737+0marAnsari@users.noreply.github.com>
2024-10-25 21:38:03 +0000

    HTML links

--- dice.rb
+++ dice.rb
@@ -2,7 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  "Hello World"
+  '<h1>Dice Roll</h1>
+  
+    <ul>
+      <li><a href = "/dice/2/6">Roll two 6-sided Dice</a></li>
+      <li><a href = "/dice/2/10">Roll two 10-sided Dice</a></li>
+      <li><a href = "/dice/1/20">Roll one 20-sided Dice</a></li>
+      <li><a href = "/dice/5/4">Roll five 4-sided Dice</a></li>
+    </ul>
+    '
 end
 
 get("/zebra") do
@@ -13,3 +21,49 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server"
 end
 
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+
+get("/dice/1/20") do
+  one_die = rand(1..20)
+  
+  outcome = "You rolled a #{one_die} for a total sum of #{one_die}"
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4) 
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, #{fifth_die} for a total of #{sum}"
+
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>"
+
+end
